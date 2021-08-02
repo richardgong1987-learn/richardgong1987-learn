d9a76a25561610b33746301d2f19ff3ae99153dc
os <admin@tencent.com>
2021-08-02 23:55:51 +0800

    demo:MylinkedList,MyQueue

---
+++ code/src/main/java/ch2/MyLinkedList.java
@@ -0,0 +1,95 @@
+package ch2;
+
+public class MyLinkedList {
+    private Node head;
+    private Node last;
+    private int size;
+
+
+    public void insert(int index, Object data) {
+        checkValided(index);
+        var insertedNode = new Node(data);
+        /**
+         * empty linked list
+         */
+        if (size == 0) {
+            head = insertedNode;
+            last = insertedNode;
+
+        } else if (index == 0) {
+            /**
+             * insert head
+             */
+            insertedNode.next = head;
+            head = insertedNode;
+        } else if (index == size) {
+            /**
+             * insert tail
+             */
+            last.next = insertedNode;
+            last = insertedNode;
+
+        } else {
+            var prevNode = this.get(index - 1);
+            insertedNode.next = prevNode.next;
+            prevNode.next = insertedNode;
+        }
+        size++;
+    }
+
+    private void checkValided(int index) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Out of range of tables Nodes");
+        }
+    }
+
+    private Node get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Out of range of tables Nodes");
+        }
+
+        var tmp = head;
+        for (var i = 0; i < index; i++) {
+            tmp = tmp.next;
+        }
+        return tmp;
+    }
+
+    public Node delete(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Out of range of tables Nodes");
+        }
+        Node deleteNode = null;
+        if (index == 0) {
+            deleteNode = head;
+            head = head.next;
+        } else if (index == size - 1) {
+            deleteNode = last;
+            var prevNode = get(index - 1);
+            prevNode.next = null;
+            last = prevNode;
+        } else {
+            var prevNode = get(index - 1);
+            deleteNode = prevNode.next;
+            prevNode.next = prevNode.next.next;
+        }
+
+        return deleteNode;
+    }
+
+    public void output() {
+        var tmp = head;
+        while (tmp != null) {
+            System.out.println(tmp.data);
+            tmp = tmp.next;
+        }
+    }
+    public class Node {
+        public Object data;
+        public Node next;
+
+        public Node(Object data) {
+            this.data = data;
+        }
+    }
+}
---
+++ code/src/main/java/ch2/MyQueue.java
@@ -0,0 +1,34 @@
+package ch2;
+
+public class MyQueue {
+    private Object[] array;
+    private int front;
+    private int rear;
+
+    public MyQueue(int capacity) {
+        array = new Object[capacity];
+    }
+
+    public void enQueue(Object element) throws Exception {
+        if ((rear + 1) % array.length == front) {
+            throw new Exception("The Queue is full");
+        }
+        array[rear] = element;
+        rear = (rear + 1) % array.length;
+    }
+
+    public Object deQueue() throws Exception {
+        if (front == rear) {
+            throw new Exception("The Queue is empty");
+        }
+        var deQueueElement = array[front];
+        front = (front + 1) % array.length;
+        return deQueueElement;
+    }
+
+    public void output() {
+        for (var i = front; i != rear; i = (i + 1) % array.length) {
+            System.out.println(array[i]);
+        }
+    }
+}
