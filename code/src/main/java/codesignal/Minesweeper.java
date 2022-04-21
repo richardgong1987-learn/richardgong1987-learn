57202f2d10ed8b51cfd3b77c4e1dd9836dd1454c
gong.hanjin <richardgong1988@gmail.com>
2022-04-21 23:54:35 +0900

    Minesweeper

--- code/src/main/java/codesignal/Minesweeper.java
+++ code/src/main/java/codesignal/Minesweeper.java
@@ -27,27 +27,6 @@ public class Minesweeper {
 		return v;
 	}
 
-	public static int[][] solutionTwo(boolean[][] matrix) {
-		int rowLen = matrix.length;
-		int colLen = matrix[0].length;
-
-		int[][] v = new int[rowLen][colLen];
-
-		int[] xStandard = {-1, 1, 0, 0, -1, 1, -1, 1};
-
-		int[] yStandard = {0, 0, -1, 1, -1, -1, 1, 1};
-
-		for (int i = 0; i < rowLen; i++) {
-			for (int j = 0; j < colLen; j++) {
-				for (int k = 0; k < 8; k++) {
-					if (check(j + xStandard[k], i + yStandard[k], colLen, rowLen) && matrix[i + yStandard[k]][j + xStandard[k]]) {
-						v[i][j]++;
-					}
-				}
-			}
-		}
-		return v;
-	}
 
 	public static void main(String[] args) {
 
---
+++ code/src/main/java/codesignal/StringsRearrangement.java
@@ -0,0 +1,63 @@
+package codesignal;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class StringsRearrangement {
+	public static void main(String[] args) {
+		StringsRearrangement stringsRearrangement = new StringsRearrangement();
+		stringsRearrangement.solution(new String[]{"aba", "bbb", "bab"});
+
+		Deque<String> temp = new LinkedList<>();
+		temp.offer("a");
+		temp.offer("b");
+		temp.offer("c");
+		System.out.println(temp);
+	}
+
+	boolean solution(String[] inputArray) {
+		Deque<String> tries = loadString(inputArray);
+		LinkedList<String> matches = new LinkedList<>();
+		matches.offer(tries.poll());
+		int len = matches.getFirst().length();
+		while (!tries.isEmpty()) {
+			int numOfTries = tries.size();
+			String first = matches.getFirst();
+			String last = matches.getLast();
+			for (int t = 0; t < numOfTries; t++) {
+				String actual = tries.poll();
+				if (exactOneDiff(first, actual, len)) {
+					matches.offerFirst(actual);
+					break;
+				} else if (exactOneDiff(last, actual, len)) {
+					matches.offerLast(actual);
+					break;
+				} else {
+					tries.offer(actual);
+				}
+			}
+			if (numOfTries == tries.size()) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+
+	boolean exactOneDiff(String target, String actual, int len) {
+		int diff = 0;
+		for (int ch = 0; ch < len; ch++) {
+			if (target.charAt(ch) != actual.charAt(ch)) diff++;
+		}
+		return diff == 1;
+	}
+
+	Deque<String> loadString(String[] in) {
+		Deque<String> temp = new LinkedList<>();
+		for (String str : in) {
+			temp.offer(str);
+		}
+		return temp;
+	}
+
+}
